ad8bac46466fbf7b1138296912f4c0046042afdf
Lpaladini90 <lpaladini@me.com>
2022-05-06 13:23:14 -0700

    build out of hunt_trip entity, packages for controller, impl, dao and repo.

---
+++ OutboundREST/src/main/java/com/skilldistillery/outbound/data/HuntTripDAO.java
@@ -0,0 +1,22 @@
+package com.skilldistillery.outbound.data;
+
+import java.util.List;
+
+import com.skilldistillery.outbound.entities.HuntTrip;
+
+public interface HuntTripDAO {
+
+	List<HuntTrip> findAll();
+	
+	HuntTrip findHuntTripById(int huntId);
+	
+	HuntTrip createHuntTrip(HuntTrip hunt);
+	
+	HuntTrip updateHuntTrip(HuntTrip hunt, int huntId);
+	
+	boolean deleteHuntTrip(int huntId);
+	
+	
+	
+	
+}
---
+++ OutboundREST/src/main/java/com/skilldistillery/outbound/data/HuntTripDaoImpl.java
@@ -0,0 +1,39 @@
+package com.skilldistillery.outbound.data;
+
+import java.util.List;
+
+import com.skilldistillery.outbound.entities.HuntTrip;
+
+public class HuntTripDaoImpl implements HuntTripDAO {
+
+	@Override
+	public List<HuntTrip> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HuntTrip findHuntTripById(int huntId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HuntTrip createHuntTrip(HuntTrip hunt) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HuntTrip updateHuntTrip(HuntTrip hunt, int huntId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean deleteHuntTrip(int huntId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
