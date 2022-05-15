89c79cebeb28172d6865a0054727844dd2fac593
Lpaladini90 <lpaladini@me.com>
2022-05-15 12:04:12 -0700

    Get huny by id, get all hunts, create hunt and delete hunt working. update hunt is all that is left

--- OutboundREST/src/main/webapp/js/eventFunctions.js
+++ OutboundREST/src/main/webapp/js/eventFunctions.js
@@ -6,11 +6,115 @@
     });
 
     function init() {
-        console.log("in init");
 
         getHunts();
-        }
+
         document.huntForm.submit.addEventListener('click', createHunt);
+
+        }
+
+
+
+// --------------------    FIND A SINGLE HUNT ----------------------------
+
+
+    function getSingleHunt(huntId){
+      let xhr = new XMLHttpRequest();
+      xhr.open("GET", 'api/hunttrips/' + huntId);
+      xhr.onreadystatechange = function(){
+          if(xhr.readyState == 4){
+              if(xhr.status === 200){
+              let hunt = JSON.parse(xhr.responseText);
+              console.log(hunt);
+              displayHunt(hunt);
+            }
+            else {
+              console.log("hunt not found");
+            }
+          }
+      };
+      xhr.send();
+    }
+
+
+    function displayHunt(hunt) {
+      let huntDiv = document.getElementById('huntData');
+      huntDiv.textContent = "";
+
+      let h1 = document.createElement('h1');
+      h1.textContent = "Hunt Info";
+      huntDiv.appendChild(h1);
+
+      let list = document.createElement('ul');
+      let id = document.createElement('li');
+      let species = document.createElement('li');
+      let description = document.createElement('li');
+      let type = document.createElement('li');
+      let sex = document.createElement('li');
+      let startDate = document.createElement('li');
+      let endDate = document.createElement('li');
+
+      id.textContent = hunt.id;
+      species.textContent = hunt.species;
+      description.textContent = hunt.description;
+      type.textContent = hunt.type;
+      sex.textContent = hunt.sex;
+      startDate.textContent = hunt.startDate;
+      endDate.textContent = hunt.endDate;
+
+      list.appendChild(id);
+      list.appendChild(species);
+      list.appendChild(description);
+      list.appendChild(type);
+      list.appendChild(sex);
+      list.appendChild(startDate);
+      list.appendChild(endDate);
+
+      huntDiv.appendChild(list);
+
+
+
+      let updateBtn = document.createElement('button');
+      updateBtn.id = hunt.id;
+      updateBtn.type = 'submit';
+      updateBtn.textContent = 'Update Hunt';
+      updateBtn.addEventListener('click', function(e){
+      console.log(updateBtn.id);
+
+      });
+
+      let deleteBtn = document.createElement('button');
+      deleteBtn.id = hunt.id;
+      deleteBtn.type = 'submit';
+      deleteBtn.textContent = 'Delete Hunt';
+      deleteBtn.addEventListener('click', function(e){
+          let xhr = new XMLHttpRequest();
+          xhr.open("DELETE", 'api/hunttrips/' + hunt.id);
+
+          xhr.setRequestHeader("Content-type", "application/json");
+
+          xhr.onreadystatechange = function(){
+            if(xhr.readyState === 4 ){
+            if (xhr.status == 200 || xhr.status == 201) { // Ok or Created
+                getHunts()
+                }
+                else {
+                  console.error("DELETE request failed.");
+        				  console.error(xhr.status + ': ' + xhr.responseText);
+                }
+              }
+        };
+
+          xhr.send(JSON.stringify(hunt));
+        });
+
+
+
+
+      huntDiv.appendChild(updateBtn);
+      huntDiv.appendChild(deleteBtn);
+    }
+// ------------------------ FIND ALL HUNTS --------------------------------
 
     function getHunts() {
       let xhr = new XMLHttpRequest();
@@ -21,14 +125,74 @@
               let hunttrips = JSON.parse(xhr.responseText);
               console.log(hunttrips);
               displayHuntTrips(hunttrips);
+
+
             }
             else {
-              console.log("Film not found");
+              console.log("hunt not found");
             }
           }
       };
       xhr.send();
     }
+
+    function displayHuntTrips(hunttrips){
+      // let hunt = e.target.hunttrips.value;
+
+      let huntsDiv = document.getElementById('huntDisplay');
+      huntsDiv.textContent = "";
+      let table = document.createElement('table');
+      table.id = 'huntsTable';
+      table.border =1;
+
+      table.cellpadding = 10;
+
+      let thead = document.createElement('thead');
+      let tr = document.createElement('tr');
+      // let hunt.id. = document.createElement('tr')
+      // hunt.id.textContent = "Id";
+
+      for(h in hunttrips[0]){
+        if(h ===  "species" || h === "description"){
+        let th = document.createElement('th');
+        th.textContent = h;
+        tr.appendChild(th);
+      }
+    }
+
+      thead.appendChild(tr);
+      table.appendChild(thead);
+
+      let tbody = document.createElement('tbody');
+
+      hunttrips.forEach(function (item){
+        let row = document.createElement('tr');
+        row.addEventListener('click', function(e){
+            displayHunt(item);
+
+        });
+
+        let id =document.createElement('td');
+        let species = document.createElement('td');
+        let description = document.createElement('td');
+
+        id.textContent = item.id;
+        species.textContent = item.species;
+        description.textContent = item.description;
+
+        row.appendChild(species);
+        row.appendChild(description);
+        tbody.appendChild(row);
+
+      });
+
+      table.appendChild(tbody);
+      huntsDiv.appendChild(table);
+
+    }
+
+
+// ------------------------ CREATE A HUNT -----------------------------
 
 
 function createHunt(e){
@@ -60,57 +224,100 @@ function createHunt(e){
     endDate: document.huntForm.endDate.value
 
   }
-
-
   xhr.send(JSON.stringify(hunt));
 };
 
 
-function displayHuntTrips(hunttrips){
-  // let hunt = e.target.hunttrips.value;
+// ---------------------------- UPDATE A HUNT -----------------------------
 
-  let huntsDiv = document.getElementById('huntDisplay');
-  huntsDiv.textContent = "";
-  let table = document.createElement('table');
-  table.id = 'huntsTable';
 
-  let thead = document.createElement('thead');
-  let headRow = document.createElement('tr');
-  // let hunt.id. = document.createElement('tr')
-  // hunt.id.textContent = "Id";
+function updateHunt(hunt){
+  e.preventDefault();
 
-  for(h in hunttrips[0]){
-    if(h ===  "species" || h === "description"){
-    let th = document.createElement('th');
-    th.textContent = h;
-    headRow.appendChild(th);
+  let xhr = new XMLHttpRequest();
+  xhr.open("PUT", 'api/hunttrips/' + hunt.id);
+
+  xhr.setRequestHeader("Content-type", "application/json");
+
+  xhr.onreadystatechange = function(){
+    if(xhr.readyState === 4 ){
+    if (xhr.status == 200 || xhr.status == 201) { // Ok or Created
+          let data = JSON.parse(xhr.responseText);
+            updateHunt(data);
+        }
+        else {
+          console.error("PUT request failed.");
+				  console.error(xhr.status + ': ' + xhr.responseText);
+        }
+      }
+};
+  hunt = {
+    id: hunt.id
+    // species: document.huntForm.species.value,
+    // sex: document.huntForm.sex.value,
+    // type: document.huntForm.type.value,
+    // description: document.huntForm.description.value,
+    // startDate: document.huntForm.startDate.value,
+    // endDate: document.huntForm.endDate.value
+
   }
-}
-  thead.appendChild(headRow);
-  table.appendChild(thead);
-
-  let tbody = document.createElement('tbody');
-
-  hunttrips.forEach(function (item){
-    let row = document.createElement('tr');
-    let species = document.createElement('td');
-    let description = document.createElement('td');
-
-    species.textContent = item.species + "  ";
-
-    description.textContent = item.description + "  "
+  xhr.send(JSON.stringify(data));
+};
 
 
+function updateHunt(hunt){
+preventDefault();
 
-    row.appendChild(species);
-    row.appendChild(description);
-    tbody.appendChild(row);
+let updateDiv = document.createElement('')
 
-  });
 
-  table.appendChild(tbody);
-  huntsDiv.appendChild(table);
-  // document.body.appendChild(table);
+let h1 = document.createElement('h1');
+
+
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (let i = 0; i < hunttrips.length; i++) {
+//   let showHideRow = document.createElement('tr');
+//   showHideRow.onlclick = "showHideRow('hidden_row[i]');"
+//
+//   let hiddenRow = document.createElement('tr');
+//   hiddenRow.id ="hidden_row".hunttrips[i];
+//   hiddenRow.class = "hidden_row"
+//
+//   let species = document.createElement('td');
+//   let description = document.createElement('td');
+//   species.textContent = hunttrips[i].species + "  ";
+//   description.textContent = hunttrips[i].description + "  "
+//
+// showHideRow.appendChild(species);
+// showHideRow.appendChild(description);
+//
+// hiddenRow.appendChild(sex);
+// hiddenRow.appendChild(type);
+// hiddenRow.appendChild(startDate);
+// hiddenRow.appendChild(endDate);
+//
+//
+//
+//
+// tbody.appendChild(hiddenRow);
+// tbody.appendChild(showHide);
+// };
+//
+// table.appendChild(tbody);
+// huntsDiv.appendChild(table);
