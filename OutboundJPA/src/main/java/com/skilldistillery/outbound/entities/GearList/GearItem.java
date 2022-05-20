5221507c0a0e12aa6f445085371d183a7613860c
Lpaladini90 <lpaladini@me.com>
2022-05-19 22:46:26 -0700

    Database edit- simplification

--- OutboundJPA/src/main/java/com/skilldistillery/outbound/entities/GearList/GearItem.java
+++ OutboundJPA/src/main/java/com/skilldistillery/outbound/entities/GearList/GearItem.java
@@ -1,18 +1,16 @@
 package com.skilldistillery.outbound.entities.gearlist;
 
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class GearList {
+public class GearItem {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
-	 
 	
 }
